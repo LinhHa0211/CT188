3e04d6b952b6ced1069c6433daf7bde2c63c6d77
TranNhan2004 <trannhanlv2004@gmail.com>
2024-04-18 09:11:10 +0700

    Nhan sua header va them trang contact.

---
+++ js/contact.js
@@ -0,0 +1,19 @@
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Get the email from contact form
+    const email = document.getElementById('email').value;
+
+    // Check if the email is valid
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(email)) {
+      alert("Wrong Email Format!!!");
+      return;
+    }
+
+    alert("Form Is Submitted Successfully");
+});
+
+
+
