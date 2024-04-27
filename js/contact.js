072372a93996343d56a87832b99ed85dd0431beb
TranNhan2004 <trannhanlv2004@gmail.com>
2024-04-27 17:05:33 +0700

    Nhan thay doi cac file

--- js/contact.js
+++ js/contact.js
@@ -1,20 +1,21 @@
-const contactForm = document.getElementById('contact-form');
-contactForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Get the email from contact form
-    const email = document.getElementById('email').value;
+function contactValidate(contactForm){
+  // Check if the email is valid
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Check if the email is valid
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(contactForm.email.value)) {
+    alert("Sai Định Dạng Email!!!\n");
+    contactForm.email.focus();
+    return false;
+  }
 
-    if (!emailPattern.test(email)) {
-      alert("Wrong Email Format!!!");
-      return;
-    }
+  if(contactForm.content.value.length < 10){
+    alert("Nội Dung Liên Hệ Phải Có Từ 10 Kí Tự Trở Lên\n");
+    contactForm.content.focus();
+    return false;
+  }
 
-    alert("Form Is Submitted Successfully");
-    location.reload();
-});
-
+  alert("Đã Nộp Đơn Liên Hệ Thành Công!");
+  return true;
+}
 
 
