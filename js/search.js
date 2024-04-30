fd9c0abfb53292ad631855c303dcfe58d61e6b22
Ha Ngoc Linh <hnlinh0211@gmail.com>
2024-04-30 20:25:45 +0700

    Linh add file

--- js/search.js
+++ js/search.js
@@ -1,40 +1,220 @@
 let products = {
     data: [
         {
-            productName: "i1",
-            category: "Mercedes",
-            price: "12",
-            image: "../images/bmw.png",
-        },
-        {
-            productName: "i2",
-            category: "Mercedes",
-            price: "214",
-            image: "../images/bmw.png",
-        },
-        {
-            productName: "i1",
+            productName: "I4",
             category: "BMW",
-            price: "124",
-            image: "../images/bmw.png",
+            price: "3.739.000.000",
+            image: "../images/product/bmw/BMW-I4-1.avif",
         },
         {
-            productName: "i2",
+            productName: "I5",
             category: "BMW",
-            price: "214",
-            image: "../images/bmw.png",
+            price: "2.599.000.000",
+            image: "../images/product/bmw/BMW-I5-1.avif",
         },
         {
-            productName: "i1",
-            category: "Toyota",
-            price: "124",
-            image: "../images/bmw.png",
+            productName: "I7",
+            category: "BMW",
+            price: "6.499.000.000",
+            image: "../images/product/bmw/BMW-I7-1.avif",
         },
         {
-            productName: "i2",
+            productName: "IX",
+            category: "BMW",
+            price: "3.229.000.000",
+            image: "../images/product/bmw/BMW-IX-1.avif",
+        },
+        {
+            productName: "M2",
+            category: "BMW",
+            price: "2.999.000.000",
+            image: "../images/product/bmw/BMW-M2-1.avif",
+        },
+        {
+            productName: "M4",
+            category: "BMW",
+            price: "4.239.000.000",
+            image: "../images/product/bmw/BMW-M4-1.avif",
+        },
+        {
+            productName: "M8",
+            category: "BMW",
+            price: "12.939.000.000",
+            image: "../images/product/bmw/BMW-M8-1.avif",
+        },
+        {
+            productName: "XM",
+            category: "BMW",
+            price: "10.999.000.000",
+            image: "../images/product/bmw/BMW-XM-1.avif",
+        },
+        {
+            productName: "X3",
+            category: "BMW",
+            price: "1.795.000.000",
+            image: "../images/product/bmw/BMW-X3-1.avif",
+        },
+        {
+            productName: "X4",
+            category: "BMW",
+            price: "2.899.000.000",
+            image: "../images/product/bmw/BMW-X4-1.avif",
+        },
+        {
+            productName: "X5",
+            category: "BMW",
+            price: "3.909.000.000",
+            image: "../images/product/bmw/BMW-X5-1.avif",
+        },
+        {
+            productName: "X6",
+            category: "BMW",
+            price: "3.799.000.000",
+            image: "../images/product/bmw/BMW-X6-1.avif",
+        },
+        {
+            productName: "X7",
+            category: "BMW",
+            price: "5.149.000.000",
+            image: "../images/product/bmw/BMW-X7-1.avif",
+        },
+        {
+            productName: "GLC",
+            category: "Mercedes",
+            price: "2.299.000.000",
+            image: "../images/product/Mercedes/Mer-GLC-3.avif",
+        },
+        {
+            productName: "GLE",
+            category: "Mercedes",
+            price: "4.359.000.000",
+            image: "../images/product/Mercedes/Mer-GLE-1.avif",
+        },
+        {
+            productName: "GLI",
+            category: "Mercedes",
+            price: "5.239.000.000",
+            image: "../images/product/Mercedes/Mer-GLI-1.avif",
+        },
+        {
+            productName: "GLS",
+            category: "Mercedes",
+            price: "5.799.000.000",
+            image: "../images/product/Mercedes/Mer-GLS-1.avif",
+        },
+        {
+            productName: "Class C",
+            category: "Mercedes",
+            price: "2.199.000.000",
+            image: "../images/product/Mercedes/Mer-CClass-1.avif",
+        },
+        {
+            productName: "Class E",
+            category: "Mercedes",
+            price: "1.899.000.000",
+            image: "../images/product/Mercedes/Mer-EClass-1.avif",
+        },
+        {
+            productName: "Class S",
+            category: "Mercedes",
+            price: "5.079.000.000",
+            image: "../images/product/Mercedes/Mer-SClass-1.avif",
+        },
+        {
+            productName: "Class GT63",
+            category: "Mercedes",
+            price: "12.499.000.000",
+            image: "../images/product/Mercedes/Mer-GT63-1.avif",
+        },
+        {
+            productName: "450",
+            category: "Mercedes",
+            price: "5.249.000.000",
+            image: "../images/product/Mercedes/Mer-450-1.avif",
+        },
+        {
+            productName: "580",
+            category: "Mercedes",
+            price: "5.966.000.000",
+            image: "../images/product/Mercedes/Mer-580-1.avif",
+        },
+        {
+            productName: "AMG",
+            category: "Mercedes",
+            price: "2.329.000.000",
+            image: "../images/product/Mercedes/Mer-AMG-1.avif",
+        },
+        {
+            productName: "EQS",
+            category: "Mercedes",
+            price: "4.839.000.000",
+            image: "../images/product/Mercedes/Mer-EQS-1.avif",
+        },
+        {
+            productName: "Mirai",
             category: "Toyota",
-            price: "214",
-            image: "../images/bmw.png",
+            price: "1.250.000.000",
+            image: "../images/product/toyota/Toyota-Mirai-1.avif",
+        },
+        {
+            productName: "Corolla",
+            category: "Toyota",
+            price: "955.000.000",
+            image: "../images/product/toyota/Toyota-Corolla-1.avif",
+        },
+        {
+            productName: "Prius",
+            category: "Toyota",
+            price: "626.000.000",
+            image: "../images/product/toyota/Toyota-Prius-1.avif",
+        },
+        {
+            productName: "Camry",
+            category: "Toyota",
+            price: "1.105.000.000",
+            image: "../images/product/toyota/Toyota-Camry-1.avif",
+        },
+        {
+            productName: "Crown",
+            category: "Toyota",
+            price: "976.000.000",
+            image: "../images/product/toyota/Toyota-Crown-1.avif",
+        },
+        {
+            productName: "Sequoia",
+            category: "Toyota",
+            price: "7.250.000.000",
+            image: "../images/product/toyota/Toyota-Sequoia-1.avif",
+        },
+        {
+            productName: "RAV4",
+            category: "Toyota",
+            price: "2.600.000.000",
+            image: "../images/product/toyota/Toyota-RAV4-1.avif",
+        },
+        {
+            productName: "Grand Highlander",
+            category: "Toyota",
+            price: "4.900.000.000",
+            image: "../images/product/toyota/Toyota-GrandHighlander-1.avif",
+        },
+        {
+            productName: "4Runner",
+            category: "Toyota",
+            price: "4.100.000.000",
+            image: "../images/product/toyota/Toyota-4Runner-1.avif",
+        },
+        {
+            productName: "Tacoma",
+            category: "Toyota",
+            price: "5.520.000.000",
+            image: "../images/product/toyota/Toyota-Tacoma-1.avif",
+        },
+        {
+            productName: "Tundra",
+            category: "Toyota",
+            price: "2.599.000.000",
+            image: "../images/product/toyota/Toyota-Tundra-1.avif",
         },
     ],
 }
@@ -61,7 +241,7 @@ for (let i of products.data) {
     container.appendChild(name);
     //Prize
     let price = document.createElement("h6");
-    price.innerText = i.price + "VND";
+    price.innerText = i.price + " VND";
     container.appendChild(price);
     
     card.appendChild(container);
